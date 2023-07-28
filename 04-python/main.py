23a6f210f00763889b629113ee49ee29204b03ef
Eugen-str <eugen.01654@gmail.com>
2023-07-28 16:40:48 +0200

    Added solution to the second puzzle of day 4.

--- 04-python/main.py
+++ 04-python/main.py
@@ -10,11 +10,8 @@ def parse_field(fields, line):
 
 def solution1(passports):
     result = 0
-    c = 0
     for passport in passports:
-        c += 1
         if verify1(passport):
-            print(c)
             result += 1
 
     return result
@@ -27,6 +24,80 @@ def verify1(passport):
         if not req in names:
             return False
 
+    return True
+
+def solution2(passports):
+    result = 0
+    for passport in passports:
+        if verify1(passport) and verify2(passport):
+            result += 1
+
+    return result
+
+def verify2(passport):
+    field_verify = {
+        "byr": verify_byr,
+        "iyr": verify_iyr,
+        "eyr": verify_eyr,
+        "hgt": verify_hgt,
+        "hcl": verify_hcl,
+        "ecl": verify_ecl,
+        "pid": verify_pid,
+        "cid": verify_cid
+    }
+
+    for field in passport:
+        if not field_verify[field.name](field.value):
+            return False
+
+    return True
+
+def verify_byr(value):
+    return 1920 <= int(value) <= 2002
+
+def verify_iyr(value):
+    return 2010 <= int(value) <= 2020
+
+def verify_eyr(value):
+    return 2020 <= int(value) <= 2030
+
+def verify_hgt(value):
+    if "cm" in value:
+        return 150 <= int(value.replace("cm", "")) <= 193
+    else:
+        return 59 <= int(value.replace("in", "")) <= 76
+
+def verify_hcl(value):
+    if not value[0] == '#' or not len(value) == 7:
+        return False
+
+    allowed = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+               'a', 'b', 'c', 'd', 'e', 'f']
+
+    for i in range(1, 5):
+        if value[i] not in allowed:
+            return False
+
+    return True
+
+def verify_ecl(value):
+    allowed = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+    if not value in allowed:
+        return False
+
+    return True
+
+def verify_pid(value):
+    if not len(value) == 9:
+        return False
+
+    for i in range(0, 9):
+        if not value[i].isdigit():
+            return False
+
+    return True
+
+def verify_cid(value):
     return True
 
 if __name__ == "__main__":
@@ -44,4 +115,5 @@ if __name__ == "__main__":
                 fields = []
         passports.append(fields)
 
-        print("\nSolution 1 : ", solution1(passports))
+        print("\nSolution 1 :", solution1(passports))
+        print("\nSolution 2 :", solution2(passports))
