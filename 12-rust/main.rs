6476c0e7b192f90478772780222e5f92e8ae8075
Eugen-str <eugen.01654@gmail.com>
2023-08-07 12:59:45 +0200

    Added solution to the second puzzle of day 12.

--- 12-rust/main.rs
+++ 12-rust/main.rs
@@ -1,5 +1,3 @@
-use std::fs;
-
 struct Ferry{
     position: (i32, i32),
     angle: i32,
@@ -8,7 +6,7 @@ struct Ferry{
 fn solution1(input: &String) -> i32{
     let mut ferry = Ferry{
         position: (0, 0),
-        angle: 0
+        angle: 0,
     };
 
     for line in input.lines(){
@@ -39,10 +37,58 @@ fn solution1(input: &String) -> i32{
     return (ferry.position.0).abs() + (ferry.position.1).abs();
 }
 
+fn solution2(input: &String) -> i32{
+    let mut ferry = Ferry{
+        position: (0, 0),
+        angle: 0,
+    };
+    let mut waypoint = (1, 10);
+
+    for line in input.lines(){
+        let command = line.chars().nth(0).expect("Error!");
+        let value: i32 = line[1..].parse::<i32>().unwrap();
+
+        match command{
+            'N' => waypoint.0 += value,
+            'S' => waypoint.0 -= value,
+            'E' => waypoint.1 += value,
+            'W' => waypoint.1 -= value,
+            'R' | 'L' => {
+                let m = if command == 'R' {1} else {-1};
+                match value{
+                    90 => {
+                        let temp = waypoint.0;
+                        waypoint.0 = -waypoint.1 * m;
+                        waypoint.1 = temp * m;
+                    },
+                    180 => {
+                        waypoint.0 = -waypoint.0;
+                        waypoint.1 = -waypoint.1;
+                    },
+                    270 => {
+                        let temp = waypoint.0;
+                        waypoint.0 = waypoint.1 * m;
+                        waypoint.1 = -temp * m;
+                    }
+                    0 => (),
+                    _ => panic!(),
+                }
+            },
+            'F' => {
+                ferry.position.0 += waypoint.0 * value;
+                ferry.position.1 += waypoint.1 * value;
+            },
+            _ => panic!(),
+        }
+    }
+    return (ferry.position.0).abs() + (ferry.position.1).abs();
+}
+
 fn main(){
     let input =
-        fs::read_to_string("input.txt")
+        std::fs::read_to_string("input.txt")
         .unwrap();
 
     println!("Solution 1 : {}", solution1(&input));
+    println!("Solution 2 : {}", solution2(&input));
 }
