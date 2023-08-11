6a2255dba4f1ebee9461417ae7122c82768dc45a
Eugen-str <eugen.01654@gmail.com>
2023-08-11 18:17:26 +0200

    Updated solution to the second puzzle.

--- 15-nim/main.nim
+++ 15-nim/main.nim
@@ -1,5 +1,7 @@
+import std/tables
+
 var
-  sample = @[0,3,6]
+  #sample = @[0,3,6]
   input = @[6,3,15,13,1,0]
 
 proc get_recent_nums(list: seq[int]): seq[tuple[num: int, idx: int]] =
@@ -7,32 +9,53 @@ proc get_recent_nums(list: seq[int]): seq[tuple[num: int, idx: int]] =
 
     let x = list[len(list) - 1]
 
-    for num in countdown(list.len - 1, 0):
-      if list[num] == x:
-        result.add((num: list[num], idx: num))
-      if len(result) == 2:
-        break
+    for i in countdown(list.len - 1, 0):
+      if list[i] == x:
+        result.add((num: list[i], idx: i))
+        if len(result) == 2:
+          break
 
 proc next_number(list: var seq[int]) =
   let next = get_recent_nums(list)
 
-  case len(next):
-    of 1:
-      list.add(0)
-    of 2:
-      list.add(next[0].idx - next[1].idx)
-    else:
-      echo "Error!!!"
+  if len(next) == 2:
+    list.add(next[0].idx - next[1].idx)
+  else:
+    list.add(0)
 
-proc solution(list: var seq[int], n: int): int =
+proc slow_solution(list: var seq[int], n: int): int =
   for i in 0..(n - len(list) - 1):
     next_number(list)
 
   return list[len(list) - 1]
 
+#[
+Slow solution took way too long for part two,
+new function takes 10,751s.
+]#
+
+proc fast_solution(list: var seq[int], n: int): int =
+  var nums = initTable[int, int]()
+  var next, prev: int
+
+  for i in 0..<n:
+    if i < len(list):
+      next = list[i]
+    else:
+      if prev in nums:
+        next = i - nums[prev]
+      else:
+        next = 0
+
+    if i != 0:
+      nums[prev] = i
+
+    prev = next
+
+  return prev
+
 proc main() =
-  echo "Solution 1 : ", solution(input, 2020)
-  # Solution 2 technically should work, but takes really long to execute using this method.
-  echo "Solution 2 : ", solution(input, 30_000_000)
+  echo "Solution 1 : ", slow_solution(input, 2020)
+  echo "Solution 2 : ", fast_solution(input, 30_000_000)
 
 main()
